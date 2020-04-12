733310cecfa44a2ec0fabba96c8fd820d85818c6
Ziping Chen <techping.chan@gmail.com>
2020-04-12 13:24:58 -0700

    update streaming model
    Signed-off-by: Ziping Chen <techping.chan@gmail.com>

--- streaming.py
+++ streaming.py
@@ -1,10 +1,15 @@
 from config import config
-from model import get_model, get_streaming_model
+from model import get_model, get_streaming_model, my_sparse_categorical_accuracy
 from data import get_data
 import tensorflow as tf
 from tqdm import tqdm
 import numpy as np
+import os
+import tensorflow.keras.backend as K
 
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+#tf.get_logger().setLevel('ERROR')
+tf.compat.v1.logging.set_verbosity(tf.compat.v1.logging.ERROR)
 gpus = tf.config.experimental.list_physical_devices('GPU')
 for gpu in gpus:
     tf.config.experimental.set_memory_growth(gpu, True)
@@ -53,30 +58,30 @@ if False:
             #print(f'{s},{n}:{np.isclose(single_pred[0], seq_pred[n])}')
     print(f'neq_count: {neq_count}')
 
-#count = 0
-#X_val = X_val.reshape((-1, 64))
-#y_val = y_val.reshape((-1, 1))
+# input random seqence length
 correct = 0
-for i in range(X_val.shape[0]):
+ommited_silence_len = 0
+for i in tqdm(range(X_val.shape[0])):
     count = 0
+    sub_correct = 0
+    sub_ommited_silence_len = 0
     while count < train_seq_length:
-        N = np.random.randint(300, 1000)
+        N = np.random.randint(300, 1000) # randomly generate sequence length
         if N + count > train_seq_length:
             N = train_seq_length - count
         X = X_val[i][count:count + N].reshape((1, -1, feature_dim))
         y = y_val[i][count:count + N].reshape((1, -1, 1))
-        acc = streaming_model.evaluate(x=X, y=y, use_multiprocessing=True)[1]
-        correct += (acc * N)
+        #pred = streaming_model.predict(X, use_multiprocessing=True, verbose=)
+        pred = streaming_model(X)
+        cor = tf.reduce_sum(my_sparse_categorical_accuracy(y, pred))
+        sub_ommited_silence_len += np.sum(tf.not_equal(tf.cast(y, tf.int32), -1 * tf.ones((1,), dtype=tf.int32)).numpy().reshape((-1)))
+        #print(f'acc: {cor / N}')
+        #acc = streaming_model.evaluate(x=X, y=y, verbose=0, use_multiprocessing=True)[1]
+        sub_correct += cor#int(acc * N)
         count += N
+        K.clear_session()
+    correct += sub_correct
+    ommited_silence_len += sub_ommited_silence_len
+    #print(f'Sequence {i+1} acc: {sub_correct / sub_ommited_silence_len}')
     streaming_model.reset_states()
-# while count < num_seqs * train_seq_length:
-#     N = np.random.randint(300, 1000)# randomly generate sequence length
-#     if N + count > num_seqs * train_seq_length: 
-#         N = num_seqs * train_seq_length  - count
-#     X = X_val[count:count+N].reshape((1, -1, feature_dim))
-#     y = y_val[count:count+N].reshape((1, -1, 1))
-#     acc = streaming_model.evaluate(x=X, y=y, use_multiprocessing=True)
-#     #print(f'\racc: {acc}', end='')
-#     correct += acc[1] * N
-#     count += N
-print(f'total streaming accuracy on validation set: {correct / (num_seqs * train_seq_length)}')
+print(f'Total streaming accuracy on validation set: {correct / ommited_silence_len}')
